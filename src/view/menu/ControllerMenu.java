0afafcaaec4816aa08436ef54e021a7646a023a0
RicardoVenancio <ricardoicastro@hotmail.com>
2020-10-12 01:00:47 -0300

    Versao 12102020 0100

--- src/view/menu/ControllerMenu.java
+++ src/view/menu/ControllerMenu.java
@@ -50,6 +50,7 @@ public class ControllerMenu implements Initializable{
         	BTNTeste.setVisible(false);
     	}
     	
+    	//TESTE BRANCH
     	else {
     		LBTeste.setVisible(true);
         	TXTTeste.setVisible(true);
